2b80ba140992a4698f6b528e2f8d8c2f7fc9eb18
Kseinya Reyes <kseinyar@stcu.org>
2021-12-01 21:14:36 -0800

    day 2

---
+++ csharp/AOC2021/AOC2021/Day2.cs
@@ -0,0 +1,66 @@
+﻿namespace AOC2021
+{
+    using System;
+
+    public static class Day2
+    {
+
+        public static void Part1()
+        {
+            var horizontal = 0;
+            var vertical = 0;
+
+            foreach (string line in System.IO.File.ReadLines(@"AppData\day2.txt"))
+            {
+                var courseValues = line.Split(' ');
+                var direction = courseValues[0];
+                var positionChange = Convert.ToInt32(courseValues[1]);
+                if(direction == "forward")
+                {
+                    horizontal += positionChange;
+                }
+                else if(direction == "down")
+                {
+                    vertical += positionChange;
+                }
+                else
+                {
+                    vertical -= positionChange;
+                }
+            }
+
+            Console.WriteLine("Part 1: " + horizontal * vertical);
+        }
+
+        public static void Part2()
+        {
+            var horizontal = 0;
+            var vertical = 0;
+            var aim = 0;
+
+            foreach (string line in System.IO.File.ReadLines(@"AppData\day2.txt"))
+            {
+                var courseValues = line.Split(' ');
+                var direction = courseValues[0];
+                var positionChange = Convert.ToInt32(courseValues[1]);
+                if (direction == "forward")
+                {
+                    horizontal += positionChange;
+                    vertical += (aim * positionChange);
+                }
+                else if (direction == "down")
+                {
+                    aim += positionChange;
+                }
+                else
+                {
+                    aim -= positionChange;
+                }
+            }
+
+            Console.WriteLine("Part 1: " + horizontal * vertical);
+        }
+
+
+    }
+}
