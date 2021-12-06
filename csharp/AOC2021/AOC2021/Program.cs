8254835d02f56d53995b5d152a733c7f0af66ba1
Kseinya Reyes <kseinyar@stcu.org>
2021-12-06 13:05:37 -0800

    wow part 2

--- csharp/AOC2021/AOC2021/Program.cs
+++ csharp/AOC2021/AOC2021/Program.cs
@@ -6,8 +6,8 @@ namespace AOC2021
     {
         static void Main(string[] args)
         {
-            Day5.Part1();
-            Day5.Part2();
+            Day6.Part1();
+            Day6.Part2();
         }
     }
 }
