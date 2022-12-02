0a55cc70256eee24c9d91ca3a6c0aa0c2b01ebae
Kseinya Reyes <kseinyar@stcu.org>
2022-12-02 09:50:37 -0800

    day 2 attempt 2

--- python/day2.py
+++ python/day2.py
@@ -1,3 +1,76 @@
+#part 1
+# score = 0
+
+# with open('fulldata.txt') as f:
+#     for line in f:
+#         opp = line[0]
+#         you = line.rstrip()[-1]
+
+#         if(you == 'X'):
+#             score += 1
+#             if(opp == 'A'):
+#                 score += 3
+#             elif(opp == 'C'):
+#                 score += 6
+#         elif(you == 'Y'):
+#             score += 2
+#             if(opp == 'B'):
+#                 score += 3
+#             elif(opp == 'A'):
+#                 score += 6
+#         elif(you == 'Z'):
+#             score += 3
+#             if(opp == 'C'):
+#                 score += 3
+#             elif(opp == 'B'):
+#                 score += 6
+
+# print(score)
+
+#part 2
+# score = 0
+
+# with open('fulldata.txt') as f:
+#     for line in f:
+#         opp = line[0]
+#         you = line.rstrip()[-1]
+
+#         if(you == 'X'):
+#             score += 0
+#             if(opp == 'A'):
+#                 score += 3
+#             elif(opp == 'B'):
+#                 score += 1
+#             elif(opp == 'C'):
+#                 score += 2
+#         elif(you == 'Y'):
+#             score += 3
+#             if(opp == 'A'):
+#                 score += 1
+#             elif(opp == 'B'):
+#                 score += 2
+#             elif(opp == 'C'):
+#                 score += 3
+#         elif(you == 'Z'):
+#             score += 6
+#             if(opp == 'A'):
+#                 score += 2
+#             elif(opp == 'B'):
+#                 score += 3
+#             elif(opp == 'C'):
+#                 score += 1
+
+# print(score)
+
+
+
+### Attempt 2 cleanup
+
+opts = ['A','B','C']
+outcomes = ['X','Y','Z']
+
+#part 1
+
 score = 0
 
 with open('fulldata.txt') as f:
@@ -5,58 +78,44 @@ with open('fulldata.txt') as f:
         opp = line[0]
         you = line.rstrip()[-1]
 
-        if(you == 'X'):
-            score += 1
-            if(opp == 'A'):
-                score += 3
-            elif(opp == 'C'):
-                score += 6
-        elif(you == 'Y'):
-            score += 2
-            if(opp == 'B'):
-                score += 3
-            elif(opp == 'A'):
-                score += 6
-        elif(you == 'Z'):
+        oppIndex = opts.index(opp)
+        youIndex = outcomes.index(you)
+
+        if(oppIndex == youIndex):
             score += 3
-            if(opp == 'C'):
-                score += 3
-            elif(opp == 'B'):
-                score += 6
+        elif(oppIndex == youIndex - 1 or (youIndex == 0 and oppIndex == 2)):
+            score += 6
+
+        score += 1 + youIndex
+
 
 print(score)
 
+#part 2
 
 score = 0
 
 with open('fulldata.txt') as f:
     for line in f:
         opp = line[0]
-        you = line.rstrip()[-1]
+        outcome = line.rstrip()[-1]
 
-        if(you == 'X'):
-            score += 0
-            if(opp == 'A'):
+        oppIndex = opts.index(opp)
+
+        score += (outcomes.index(outcome) * 3)
+
+        if(outcome == 'X'):
+            if(oppIndex == 0):
                 score += 3
-            elif(opp == 'B'):
+            else:
+                score += oppIndex
+        elif(outcome == 'Y'):
+            score += oppIndex + 1
+        elif(outcome == 'Z'):
+            if(oppIndex == 2):
                 score += 1
-            elif(opp == 'C'):
-                score += 2
-        elif(you == 'Y'):
-            score += 3
-            if(opp == 'A'):
-                score += 1
-            elif(opp == 'B'):
-                score += 2
-            elif(opp == 'C'):
-                score += 3
-        elif(you == 'Z'):
-            score += 6
-            if(opp == 'A'):
-                score += 2
-            elif(opp == 'B'):
-                score += 3
-            elif(opp == 'C'):
-                score += 1
+            else:
+                score += oppIndex + 2
+
 
 print(score)
