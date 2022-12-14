393376b4fe8e42dccdaf51dfdc8915b993d9910f
Kseinya Reyes <kseinyar@stcu.org>
2022-12-14 09:39:10 -0800

    day 14 organized into functions but still not good

--- python/day14.py
+++ python/day14.py
@@ -8,70 +8,88 @@ def read_data():
 
 data = read_data()
 
-grid = []
-lines = set()
 
 minX = 1000
-maxX = 0
+maxX, maxY = 0, 0
 
-maxY = 0
 
 def printBlocks():
-    for y, block in enumerate(blocks):
+    for block in enumerate(blocks):
         tempz = ''
-        for x, b in enumerate(block):
+        for b in enumerate(block):
             if(b):
                 tempz += ('#')
             else:
                 tempz += (' ')
         print(tempz)
 
-for line in data:
-    sp = line.split(' -> ')
-    points = []
-    for i in sp:
-        c = i.split(',')
-        point = (int(c[0]), int(c[1]))
-        points.append(point)
-        lines.add(point)
-        minX = min(minX, point[0])
-        maxX = max(maxX, point[0])
-        maxY = max(maxY, point[1])
+def getAllLines():
+    global minX, maxX, maxY
+    lines = set()
 
-    for p, point in enumerate(points):
-        if(p == len(points) -1):
-            continue
-        xs = list(range(point[0], points[p+1][0], 1))
-        xs.extend(range(points[p+1][0], point[0],  1))
-        ys = list(range(point[1], points[p+1][1], 1))
-        ys.extend(range(points[p+1][1], point[1],  1))
+    for line in data:
+        sp = line.split(' -> ')
+        points = []
+        for i in sp:
+            c = i.split(',')
+            point = (int(c[0]), int(c[1]))
+            points.append(point)
+            lines.add(point)
+            minX = min(minX, point[0])
+            maxX = max(maxX, point[0])
+            maxY = max(maxY, point[1])
 
-        for x in xs:
-            lines.add((x, point[1]))
+        for p, point in enumerate(points):
+            if(p == len(points) -1):
+                continue
+            xs = list(range(point[0], points[p+1][0], 1))
+            xs.extend(range(points[p+1][0], point[0],  1))
+            ys = list(range(point[1], points[p+1][1], 1))
+            ys.extend(range(points[p+1][1], point[1],  1))
 
-        for y in ys:
-            lines.add((point[0], y))
+            for x in xs:
+                lines.add((x, point[1]))
 
+            for y in ys:
+                lines.add((point[0], y))
+    
+    return lines
 
-for y in range(maxY+1):
-    temp = []
-    for x in range(minX, maxX+1):
-        if((x,y) in lines):
-            temp.append('#')
+def sandFall(x, y):
+    if(blocks[y+1][x]):
+        #try left
+        if(blocks[y+1][x-1]):
+            #try right
+            if(blocks[y+1][x+1]):
+                return ((x, y), False)
+            else:
+                point = (x+1, y+1)
         else:
-            temp.append('.')
+            point = (x-1, y+1)
+    else:
+        point = (x, y+1)
     
-    grid.append(temp)
+    return (point, True)
 
-blocks = []
+def checkBlockBounds(x, y):
+    return (y >= len(blocks) -1 or x < 0 or x >= len(blocks[0]) -1)
 
-for g in grid:
-    temp = []
-    for x in g:
-        temp.append(x == '#')
+def getBlocks():
+    blocks = []
+    for y in range(maxY+1):
+        temp = []
+        for x in range(minX, maxX+1):
+            if((x,y) in lines):
+                temp.append(True)
+            else:
+                temp.append(False)
+        
+        blocks.append(temp)
     
-    blocks.append(temp)
+    return blocks
 
+lines = getAllLines()
+blocks = getBlocks()
 
 dropX = (500 - minX)
 dropY = 0
@@ -91,31 +109,21 @@ while(not abyss):
         y = point[1]
         x = point[0]
 
-        if(y >= len(blocks) -1 or x < 0 or x >= len(blocks[0]) -1):
-            blocks[y][x] = True
+        if(checkBlockBounds(x, y)):
             sandMoves = False
             break
 
-        if(blocks[y+1][x] == True):
-            #try left
-            if(blocks[y+1][x-1] == True):
-                #try right
-                if(blocks[y+1][x+1] == True):
-                    sandMoves = False
-                else:
-                    point = (x+1, y+1)
-            else:
-                point = (x-1, y+1)
-        else:
-            point = (x, y+1)
+        point, sandMoves = sandFall(x, y)
 
         y = point[1]
         x = point[0]
 
     blocks[y][x] = True
-    if(y >= len(blocks) or x < 0 or x >= len(blocks[0]) -1):
+
+    if(checkBlockBounds(x, y)):
         abyss = True
         break
+
     sandCount += 1
 
 
@@ -124,44 +132,50 @@ print(sandCount)
 
 #part 2
 
-blocks = []
-
 # good lord I should have just made this huge to begin with
 sides = (math.floor(maxX - minX) * 2) + 11
 
-for g in grid:
-    temp = []
-    for i in range(sides):
-        temp.append(False)
-    for x in g:
-        temp.append(x == '#')
-    for i in range(sides):
-        temp.append(False)
+def getBlocks2():
     
+    blocks = []
+    for y in range(maxY+1):
+        temp = []
+        for i in range(sides):
+            temp.append(False)
+        for x in range(minX, maxX+1):
+            if((x,y) in lines):
+                temp.append(True)
+            else:
+                temp.append(False)
+        for i in range(sides):
+            temp.append(False)
+        
+        blocks.append(temp)
+    
+    temp = []
+    for i in range(len(blocks[0])):
+        temp.append(False)
+
     blocks.append(temp)
 
-temp = []
-for i in range(len(blocks[0])):
-    temp.append(False)
+    temp = []
+    for i in range(len(blocks[0])):
+        temp.append(True)
 
-blocks.append(temp)
+    blocks.append(temp)
+    
+    return blocks
 
-temp = []
-for i in range(len(blocks[0])):
-    temp.append(True)
-
-blocks.append(temp)
-
-
+blocks = getBlocks2()
 
 dropX = (500 - minX) + sides
 dropY = 0
 
 sandCount = 0
-abyss = False
+blocked = False
 
 
-while(not abyss):
+while(not blocked):
     sandMoves = True
 
     point = (dropX, dropY)
@@ -172,36 +186,20 @@ while(not abyss):
         y = point[1]
         x = point[0]
 
-        if(y >= len(blocks) -1 or x <= 0 or x >= len(blocks[0]) -1):
-            blocks[y][x] = True
+        if(checkBlockBounds(x, y)):
             break
 
-        if(blocks[y+1][x] == True):
-            #try left
-            if(blocks[y+1][x-1] == True):
-                #try right
-                if(blocks[y+1][x+1] == True):
-                    sandMoves = False
-                else:
-                    point = (x+1, y+1)
-            else:
-                point = (x-1, y+1)
-        else:
-            point = (x, y+1)
+        point, sandMoves = sandFall(x, y)
 
         y = point[1]
         x = point[0]
-    
-    if(x == dropX and y == 0):
-        print('here')
-        abyss = True
-        break
+
 
     blocks[y][x] = True
-    # if(y >= len(blocks) or x < 0 or x >= len(blocks[0]) -1):
-    #     abyss = True
-        # break
-    # print(str(y) + " " + str(x))
+    if(checkBlockBounds(x, y) or (x == dropX and y == 0)):
+        blocked = True
+        break
+
     sandCount += 1
 
 
