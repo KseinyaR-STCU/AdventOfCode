14c0e5a71fe54793ae9d4e44aae94fd489ac1582
Kseinya Reyes <kseinyar@stcu.org>
2023-11-30 22:27:39 -0800

    2023 day 1 - the solution was staring me in the face

--- rust/2023/day01.rs
+++ rust/2023/day01.rs
@@ -4,7 +4,7 @@ mod files;
 use std::env;
 
 fn get_file() -> String {
-    return if let Some(arg1) = env::args().nth(1) {
+    return if let Some(_arg1) = env::args().nth(1) {
         "full.txt".to_string()
     }
     else {
@@ -18,28 +18,71 @@ fn main() {
     if let Ok(lines) = files::read_lines(get_file()) {
         for line in lines {
             if let Ok(ip) = line {
-                let new = ip.parse::<u32>();
-
-                if let Ok(n) = new {
-                    values.push(n);                    
-                }
+                values.push(ip);                    
             }
         }
     }
 
-    part1(values);
-    // or clone to run both parts at once
-    //part1(values.clone());
-
-    //part2(values);
+    part1(values.clone());
+    part2(values);
 }
 
-fn part1(values: Vec<u32>) {
+fn part1(values: Vec<String>) {
+    let mut total = 0;
+    for v in values {
+        let short = get_num(v);
 
-    println!("part 1: {}", values.len());
+        let new = short.parse::<u32>();
+
+        if let Ok(n) = new {
+           total = total + n;                    
+        }
+    }
+
+    println!("part 1: {}", total);
 }
 
-fn part2(values: Vec<u32>) {
+fn part2(values: Vec<String>) {
+    let mut total = 0;
+    for v in values {
+        let new_v =
+            v.replace("twone", "twoone")
+            .replace("oneight", "oneeight")
+            .replace("eightwo", "eighttwo")
+            .replace("eighthree", "eightthree")
+            .replace("threeight", "threeeight")
+            .replace("nineight", "nineeight")
+            .replace("fiveight", "fiveeight")
+            .replace("sevenine", "sevennine");
 
-    println!("part 1: {}", values.len())
+        let newest =
+            new_v.replace("one", "1")
+            .replace("two", "2")
+            .replace("three", "3")
+            .replace("four", "4")
+            .replace("five", "5")
+            .replace("six", "6")
+            .replace("seven", "7")
+            .replace("eight", "8")
+            .replace("nine", "9");
+
+        let short = get_num(newest);
+
+        let new = short.parse::<u32>();
+
+        if let Ok(n) = new {
+            total = total + n;                    
+        }
+    }
+    
+    println!("part 2: {}",total)
+}
+
+fn get_num(string_value : String) -> String {
+    let digits : String = string_value.chars().filter(|c| c.is_digit(10)).collect();
+
+    let b  = (digits.as_bytes()[0]) as char;
+    let c  = (digits.as_bytes()[digits.len() - 1]) as char;
+
+    return b.to_string() + &c.to_string();
 }
