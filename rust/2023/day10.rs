0adff0221771ba05f6327dc3d57b2f397e8e1d1c
Kseinya Reyes <kseinyar@stcu.org>
2023-12-13 15:40:40 -0800

    2023 day 10 - I can't believe I forgot about the stupid S

--- rust/2023/day10.rs
+++ rust/2023/day10.rs
@@ -1,32 +1,29 @@
 #[path = "../modules/files.rs"]
 mod files;
 
+use std::collections::HashSet;
 use std::env;
 
 fn get_file() -> String {
     return if let Some(_arg1) = env::args().nth(1) {
         "full.txt".to_string()
-    }
-    else {
+    } else {
         "test.txt".to_string()
     };
 }
 
 fn main() {
-    let values : Vec<String> = files::read_lines(get_file())
+    let values: Vec<String> = files::read_lines(get_file())
         .unwrap()
         .map(|line| line.unwrap())
         .collect();
 
-    part1(values);
-    // or clone to run both parts at once
-    //part1(values.clone());
+    let points_in_path = part1(&values);
 
-    //part2(values);
+    part2(&values, &points_in_path);
 }
 
-fn part1(values: Vec<String>) {
-
+fn part1(values: &Vec<String>) -> HashSet<(usize, usize)> {
     let mut start = (0, 0);
 
     for (i, v) in values.iter().enumerate() {
@@ -36,12 +33,14 @@ fn part1(values: Vec<String>) {
     }
 
     let mut step = 0;
-    let mut direction = 0;
-    
+    let mut direction = 2;
+
     //up 0
     //right 1
     //down 2
     //left 3
+
+    let mut points_in_path = HashSet::new();
 
     while step < 100000 {
         let (next, next_dir) = match direction {
@@ -49,69 +48,103 @@ fn part1(values: Vec<String>) {
             1 => right(start, &values),
             2 => down(start, &values),
             3 => left(start, &values),
-            _ => end(&step)
+            _ => end(&step),
         };
-        if next.0 == 1000 && start.0 == 1000 {
-             return;
+        if next.0 == 1000 && next.1 == 1000 {
+            return points_in_path;
         }
+        points_in_path.insert(next);
         direction = next_dir;
         start = next;
         step += 1;
     }
+    return points_in_path;
+}
+
+fn part2(values: &Vec<String>, points_in_path: &HashSet<(usize, usize)>) {
+
+    let mut points_in = 0;
+    let mut inside = false;
+    let mut last_turn = ' ';
+
+    let mut this_line = "".to_owned();
+
+    for (i, value) in values.iter().enumerate() {
+        inside = false;
+        for (j, v) in value.chars().enumerate() {
+            let new_point = (i, j);
+            if points_in_path.contains(&new_point) {
+                match(last_turn, v) {
+                    ('F', 'J') => { inside = !inside },
+                    ('L', '7') => { inside = !inside },
+                    (_, '-') => { },
+                    (_, '|') => { inside = !inside },
+                    (_, 'S') => { inside = !inside },
+                    (_, _) => {}
+                }
+
+                if v == 'F' || v == 'J' || v == 'L' || v == '7' {
+                    last_turn = v;
+                }
+            }
+            else if inside {
+                points_in += 1;
+            }
+        }
+    }
+    println!("part 2: {}", points_in);
 }
 
 fn end(step: &u32) -> ((usize, usize), i8) {
-    println!("total steps: {:?}", step);
     println!("part 1: {:?}", step / 2);
     return ((1000, 1000), -1);
 }
 
-fn right(start: (usize, usize),  values: &Vec<String>) -> ((usize, usize), i8) {
+fn right(start: (usize, usize), values: &Vec<String>) -> ((usize, usize), i8) {
     let next = (start.0, start.1 + 1);
-    let right_char = get_char(next,  values);
+    let right_char = get_char(next, values);
 
     return match right_char {
         '-' => (next, 1),
         'J' => (next, 0),
         '7' => (next, 2),
-        _ => (next, -1)
+        _ => (next, -1),
     };
 }
 
-fn up(start: (usize, usize),  values: &Vec<String>) -> ((usize, usize), i8) {
+fn up(start: (usize, usize), values: &Vec<String>) -> ((usize, usize), i8) {
     let next = (start.0 - 1, start.1);
-    let up_char = get_char(next,  values);
+    let up_char = get_char(next, values);
 
     return match up_char {
         'F' => (next, 1),
         '|' => (next, 0),
         '7' => (next, 3),
-        _ => (next, -1)
+        _ => (next, -1),
     };
 }
 
-
-fn down(start: (usize, usize),  values: &Vec<String>) -> ((usize, usize), i8) {
+fn down(start: (usize, usize), values: &Vec<String>) -> ((usize, usize), i8) {
     let next = (start.0 + 1, start.1);
-    let down_char = get_char(next,  values);
+    let down_char = get_char(next, values);
 
     return match down_char {
         'L' => (next, 1),
         '|' => (next, 2),
         'J' => (next, 3),
-        _ => (next, -1)
+        _ => (next, -1),
     };
 }
 
-fn left(start: (usize, usize),  values: &Vec<String>) -> ((usize, usize), i8) {
+fn left(start: (usize, usize), values: &Vec<String>) -> ((usize, usize), i8) {
     let next = (start.0, start.1 - 1);
-    let left_char = get_char(next,  values);
+    let left_char = get_char(next, values);
 
     return match left_char {
         '-' => (next, 3),
         'L' => (next, 0),
         'F' => (next, 2),
-        _ => (next, -1)
+        _ => (next, -1),
     };
 }
 
@@ -121,8 +154,3 @@ fn get_char(point: (usize, usize), values: &Vec<String>) -> char {
     }
     values[point.0].as_bytes()[point.1] as char
 }
-
-// fn part2(values: Vec<u32>) {
-
-//     println!("part 2: {}", values.len())
-// }
